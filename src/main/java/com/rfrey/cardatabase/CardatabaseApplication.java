6c362540befdec2e950546518b7118f2c2789aeb
RaphaelFrey <raphael.marius.frey@gmail.com>
2024-04-28 15:40:59 +0200

    Cleanup in CardatabaseApplication.java

--- src/main/java/com/rfrey/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/rfrey/cardatabase/CardatabaseApplication.java
@@ -1,27 +1,14 @@
 package com.rfrey.cardatabase;
 
-import com.rfrey.cardatabase.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class CardatabaseApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
-
-	private final CarRepository repository;
-	private final OwnerRepository ownerRepository;
-	private final AppUserRepository appUserRepository;
-
-    public CardatabaseApplication(CarRepository repository, OwnerRepository ownerRepository, AppUserRepository appUserRepository) {
-        this.repository = repository;
-        this.ownerRepository = ownerRepository;
-		this.appUserRepository = appUserRepository;
-    }
 
     public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
